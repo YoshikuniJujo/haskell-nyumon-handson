791c78dae2ee9c43402c5dacec10778158d20adc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-06-26 08:21:01 +0900

    2. リストによる「くりかえし」のサンプルコードを追加

--- samples/repetition.hs
+++ samples/repetition.hs
@@ -4,3 +4,9 @@ nums = filter ((== 0) . (`mod` 5)) $ map (^ 2) [1 ..]
 headTail :: [a] -> Maybe (a, [a])
 headTail (x : xs) = Just (x, xs)
 headTail [] = Nothing
+
+multiplied :: [Integer]
+multiplied =
+	(`concatMap` [1, 2, 3]) $ \x ->
+	(`concatMap` [4, 5, 6]) $ \y ->
+	(`map` [7, 8]) $ \z -> x * y * z
