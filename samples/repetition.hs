7850bf114e23fe8008bd24e6fff731a32e9bdb7e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-06-26 09:45:31 +0900

    2. リストによる「くりかえし」のサンプルコードを追加

--- samples/repetition.hs
+++ samples/repetition.hs
@@ -1,3 +1,12 @@
+sumN :: Integer -> Integer
+sumN n = sum [1 .. n]
+
+sumNot3N :: Integer -> Integer
+sumNot3N n = sum $ filter ((/= 0) . (`mod` 3)) [1 .. n]
+
+sumMod5N :: Integer -> Integer
+sumMod5N n = sum $ map (`mod` 5) [1 .. n]
+
 nums :: [Integer]
 nums = filter ((== 0) . (`mod` 5)) $ map (^ 2) [1 ..]
 
@@ -9,4 +18,7 @@ multiplied :: [Integer]
 multiplied =
 	(`concatMap` [1, 2, 3]) $ \x ->
 	(`concatMap` [4, 5, 6]) $ \y ->
-	(`map` [7, 8]) $ \z -> x * y * z
+	(`concatMap` [7, 8]) $ \z -> [x * y * z]
+
+multipliedCmp :: [Integer]
+multipliedCmp = [ x * y * z | x <- [1, 2, 3], y <- [4, 5, 6], z <- [7, 8] ]
