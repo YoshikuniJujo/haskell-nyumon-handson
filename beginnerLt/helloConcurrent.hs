98ba99082d68d558630727c01b3a34c9e722b59a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-08-27 14:21:34 +0900

    concurrent sample: from foo bar to hello world

--- beginnerLt/helloConcurrent.hs
+++ beginnerLt/helloConcurrent.hs
@@ -3,12 +3,12 @@ import Control.Concurrent (forkIO, threadDelay)
 main :: IO ()
 main = do
 	forkIO . loop $ do
-		threadDelay 2000000
-		putStrLn "foo"
+		threadDelay $ 1000 * 1000
+		putStrLn "hello"
 	forkIO . loop $ do
-		threadDelay 3000000
-		putStrLn "bar"
-	threadDelay 15000000
+		threadDelay $ 1500 * 1000
+		putStrLn "world"
+	threadDelay $ 30 * 1000 * 1000
 
 loop :: IO a -> IO ()
 loop act = act >> loop act
