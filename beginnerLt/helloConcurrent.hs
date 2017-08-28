92ac763ff9d6d59b91d970e58c7be98766194e73
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-08-28 17:05:59 +0900

    hello concurrent: use forever instead of loop

--- beginnerLt/helloConcurrent.hs
+++ beginnerLt/helloConcurrent.hs
@@ -1,14 +1,12 @@
+import Control.Monad (forever)
 import Control.Concurrent (forkIO, threadDelay)
 
 main :: IO ()
 main = do
-	forkIO . loop $ do
+	forkIO . forever $ do
 		threadDelay $ 1000 * 1000
 		putStrLn "hello"
-	forkIO . loop $ do
+	forkIO . forever $ do
 		threadDelay $ 1500 * 1000
 		putStrLn "world"
 	threadDelay $ 30 * 1000 * 1000
-
-loop :: IO a -> IO ()
-loop act = act >> loop act
